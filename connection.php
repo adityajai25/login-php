ab4363f623f07afeb987fb52855d2fa4b5a914d2
adityajai25 <adityajai243@gmail.com>
2023-03-25 17:08:58 +0530

    connection establishment

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
   define('DB_SERVER','localhost');
-  define('DB_PASSWORD','');
+  define('DB_PASSWORD','Hanusai@2002');
   define('DB_USERNAME','root');
   define('DB_DATABASE','login');
   $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
