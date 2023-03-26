1a07d6e0b3c98a5f9e27e447e7c7e30a20572c44
adityajai25 <adityajai243@gmail.com>
2023-03-26 12:23:49 +0530

    landing

--- landing.php
+++ landing.php
@@ -1,3 +1,13 @@
+<?php include 'connection.php';
+    if (isset($_SESSION['email'])) {
+    $resut = mysqli_query($conn, "SELECT * FROM users WHERE Email = $email" . $_SESSION['email'] . "'");
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        echo "<li class='login current2'><a href='login.php'>". $row['fnidusers'] . $row['lnidusers'] ."</a></li>";
+    }
+} else
+    echo "<li class='login current2'><a href='login.php'>Login / Sign up</a></li>";
+?>
 <html>
     <head>
         <title>Welcome</title>
